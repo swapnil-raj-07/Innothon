b66eee615d688fbb8bc9c54892c8e8880a3dda7f
Rahul Raj <rahul.raj-kochgs@fhr.com>
2024-08-05 17:05:26 +0530

    send-message update

--- frontend-template/src/sections/user/view/message-view.jsx
+++ frontend-template/src/sections/user/view/message-view.jsx
@@ -8,12 +8,19 @@ import Container from '@mui/material/Container';
 
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
 
 import Iconify from 'src/components/iconify';
 
 // ----------------------------------------------------------------------
 
 export default function MessagePage() {
+
+  const [timing, setTiming] = React.useState('AfterLogin');
+  const [group, setGroup] = React.useState('All');
+
   return (
     <Container>
       <Card>
@@ -24,8 +31,46 @@ export default function MessagePage() {
           <TextField name="header" label="Header" required />
           <TextField name="message" label="Message" required multiline rows={4} />
           <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-            <TextField name="pointer" label="Pointer" type="number" required />
+            
+            <Stack>
+              <InputLabel>Publishing Time</InputLabel>
+              <TextField name="publishingTime" type="datetime-local" required />
+            </Stack>
 
+            <Stack>
+              <InputLabel id="demo-simple-select">Group</InputLabel>
+              <Select
+                id="demo-simple-select"
+                label="Group"
+                value={group}
+                // onChange={handleChange}
+              >
+                <MenuItem value="All">All</MenuItem>
+                <MenuItem value="GP">GP</MenuItem>
+                <MenuItem value="KAES">KAES</MenuItem>
+                <MenuItem value="Infor">Infor</MenuItem>
+              </Select>
+            </Stack>
+
+            <Stack>
+              <InputLabel id="demo-simple-select-2">Timing</InputLabel>
+            <Select
+              id="demo-simple-select-2"
+              label="Timing"
+              value={timing}
+              // onChange={handleChange}
+            >
+              <MenuItem value="Noon">Noon</MenuItem>
+              <MenuItem value="Evening">Evening</MenuItem>
+              <MenuItem value="AfterLogin">After Login</MenuItem>
+            </Select>
+            </Stack>
+            
+          </Stack>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+              <TextField name="pointer" label="Pointer" type="number" required />
+            
+            
             <Button
               variant="contained"
               color="inherit"
